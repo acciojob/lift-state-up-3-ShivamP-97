97cfb47b048562a51a40ecf092b1f54474515a90
= <=>
2025-12-12 15:37:55 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [selectedOption, setSelectedOption] = useState(""); // Parent state
 
-export default App
+  return (
+    <div className="parent">
+      <h1>Selected Option: {selectedOption || "None"}</h1>
+
+      {/* ChildComponent1 */}
+      <div>
+        <button onClick={() => setSelectedOption("Option 1")}>
+          Select Option 1
+        </button>
+      </div>
+
+      {/* ChildComponent2 */}
+      <div>
+        <button onClick={() => setSelectedOption("Option 2")}>
+          Select Option 2
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
